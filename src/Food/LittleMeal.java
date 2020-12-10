e7cb9a6efac23c46f85f665e2796c85421080f95
ptrickyan <70837843+ptrickyan@users.noreply.github.com>
2020-12-10 10:00:17 +0800

    Update LittleMeal.java

--- src/Food/LittleMeal.java
+++ src/Food/LittleMeal.java
@@ -2,7 +2,14 @@ package Food;
 
 public class LittleMeal extends Food{
 
-	
+	/**
+     * Purpose: this is the constructor of the LittleMeal.
+     *
+     * @param  None.
+     *
+     * @return None.
+     * 
+     */
 	public LittleMeal() {
 		super(FoodType.Meal, 20, 1, 20);
 	}
