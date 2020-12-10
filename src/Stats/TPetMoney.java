a09955296278cb803a77f47f38443b8112e3b0fd
ptrickyan <70837843+ptrickyan@users.noreply.github.com>
2020-12-10 10:03:54 +0800

    Update TPetMoney.java

--- src/Stats/TPetMoney.java
+++ src/Stats/TPetMoney.java
@@ -5,14 +5,37 @@ public class TPetMoney extends TPetStat{
 		super(100, 1);
 	}
 	
+	
+	/**
+     * Purpose: this method is going to update the static of the money.
+     *
+     * @param  None.
+     *
+     * @return None.
+     * 
+     */
 	public void update() {
 		
 	}
-	
+	/**
+     * Purpose: this method is going to increase the Money.
+     *
+     * @param  the amount is the current Money double.
+     *
+     * @return None.
+     * 
+     */
 	public void earnMoney(double amount) {
 		data += amount;
 	}
-	
+	/**
+     * Purpose: this method is going to decrease the Money.
+     *
+     * @param  the amount is the current Money double.
+     *
+     * @return None.
+     * 
+     */
 	public void spendMoney(double d) {
 		data -= d;
 	}
