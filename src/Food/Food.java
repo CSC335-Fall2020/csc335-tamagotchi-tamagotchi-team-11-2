5e44ffa3fc0c25356ec42f67816b1b6ab973b706
ptrickyan <70837843+ptrickyan@users.noreply.github.com>
2020-12-10 09:58:37 +0800

    Update Food.java

--- src/Food/Food.java
+++ src/Food/Food.java
@@ -13,6 +13,16 @@ public class Food {
 
 	private double price; 
 	
+	
+	/**
+     * Purpose: this is the constructor of the Food.
+     *
+     * @param  type is the FoodType the hungryIncrease and  happinessIncrease is the double of how
+     * it can change the static, the price is double of the food.
+     *
+     * @return None.
+     * 
+     */
 	public Food(FoodType type, double hungryIncrease, double happinessIncrease, double price) {
 		this.type = type;
 		this.hungryIncrease = hungryIncrease;
@@ -20,18 +30,51 @@ public class Food {
 		this.price = price;
 	}
 	
+	/**
+     * Purpose: this is going to return the hungryIncrease.
+     *
+     * @param  None.
+     *
+     * @return hungryIncrease is the double.
+     * 
+     */
 	public double getHungryIncrease() {
 		return hungryIncrease;
 	}
 	
+	
+	/**
+     * Purpose: this is going to return the happinessIncrease.
+     *
+     * @param  None.
+     *
+     * @return happinessIncrease is the double.
+     * 
+     */
 	public double getHappinessIncrease() {
 		return happinessIncrease;
 	}
 	
+	/**
+     * Purpose: this is going to get the food type.
+     *
+     * @param  None.
+     *
+     * @return type is the FoodType.
+     * 
+     */
 	public FoodType getType() {
 		return type;
 	}
 	
+	/**
+     * Purpose: this is going to get the price.
+     *
+     * @param  None.
+     *
+     * @return price is the double.
+     * 
+     */
 	public double getPrice() {
 		return price;
 	}
