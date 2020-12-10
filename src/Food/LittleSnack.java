e158774dd503e6daba468c3fda035210602f1b4f
ptrickyan <70837843+ptrickyan@users.noreply.github.com>
2020-12-10 10:00:38 +0800

    Update LittleSnack.java

--- src/Food/LittleSnack.java
+++ src/Food/LittleSnack.java
@@ -2,6 +2,14 @@ package Food;
 
 public class LittleSnack extends Food{
 	
+	/**
+     * Purpose: this is the constructor of the LittleSnack.
+     *
+     * @param  None.
+     *
+     * @return None.
+     * 
+     */
 	public LittleSnack() {
 		super(FoodType.Snack, 5, 5, 10);
 	}
