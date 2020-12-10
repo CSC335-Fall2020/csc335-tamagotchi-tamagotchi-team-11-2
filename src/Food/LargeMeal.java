2f73919586f6cff496b6e970d32ed9b3b11d3cf3
ptrickyan <70837843+ptrickyan@users.noreply.github.com>
2020-12-10 09:59:19 +0800

    Update LargeMeal.java

--- src/Food/LargeMeal.java
+++ src/Food/LargeMeal.java
@@ -2,7 +2,14 @@ package Food;
 
 public class LargeMeal extends Food{
 
-	
+	/**
+     * Purpose: this is the constructor of the LargeMeal.
+     *
+     * @param  None.
+     *
+     * @return None.
+     * 
+     */
 	public LargeMeal() {
 		super(FoodType.Meal, 30, 2, 30);
 	}
